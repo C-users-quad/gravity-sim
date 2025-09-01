3b757f1328953ea066b451f5748455877c052851
C-users-quad <alexeatsburger@gmail.com>
2025-09-01 00:31:18 -0400

    fixed camera zoom issues

--- shaders/vertex_shader.glsl
+++ shaders/vertex_shader.glsl
@@ -16,8 +16,10 @@ out float fragRadius;
 
 void main() 
 {
+    vec2 w_centerOffset = vec2(u_WindowWidth / 2, u_WindowHeight / 2);
+    vec2 camRelativeCenter = (center - u_CamPos) * u_CamZoom;
     vec2 scaled = aPos * radius * u_CamZoom * 2.0;
-    vec2 worldPos = scaled + center - u_CamPos;
+    vec2 worldPos = camRelativeCenter + scaled + w_centerOffset;
     vec2 ndc = vec2(
         worldPos.x / u_WindowWidth * 2.0 - 1.0,
         1.0 - worldPos.y / u_WindowHeight * 2.0
